f5dafde64a005262f3e07e6d1cc2acd1d5b8864b
MoleOfWar <moleofwars@gmail.com>
2019-12-10 08:16:47 +0100

    Basic selection page
    Now using checkbox for sorting results. Basic layout for the products and beginning for details page

--- www/Printers/src/php/index.php
+++ www/Printers/src/php/index.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Accueil</title>
 </head>
+
 <body>
     <form action="products.php" method="post">
-        <input type="submit" name="">
-
+        <div style="display: inline;">
+            <p style="width: 100px;">
+                <label for="ascending">Ascendant</label>
+                <input type="radio" name="order" id="ascending" value="ascending">
+            </p>
+            <p style="width: 100px;">
+                <label for="descending">Descendant</label>
+                <input type="radio" name="order" id="descending" value="descending">
+            </p>
+        </div>
+        <button name="sorting" value="brand">Par marques</button>
+        <button name="sorting" value="size">Taille</button>
+        <button name="sorting" value="weight">Poids</button>
+        <button name="sorting" value="manufacturer">Par constructeurs</button>
+        <button name="sorting" value="top5Sell">Meilleures ventes</button>
+        <button name="sorting" value="printSpeedBW">Vitesse impression NB</button>
+        <button name="sorting" value="printSpeedCol">Vitesse impression Couleur</button>
+        <button name="sorting" value="scanResolution">Résolution scan</button>
+        <button name="sorting" value="topPrice">3 top prix</button>
+        <button name="sorting" value="priceAndManufacturer">Par constructeurs ET prix</button>
     </form>
 </body>
+
 </html>
--- www/Printers/src/php/products.php
+++ www/Printers/src/php/products.php
@@ -1,12 +1,127 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Produits</title>
 </head>
+
 <body>
-    
+    <?php
+    include_once("database.php");
+    $db = new Database();
+    $data = array();
+
+    switch ($_POST["sorting"]) {
+        case "brand":
+            echo 'Par marques';
+            break;
+        case "size":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Taille';
+            break;
+        case "weight":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Poids';
+            break;
+        case "manufacturer":
+            echo 'Par constructeurs';
+            break;
+        case "top5Sell":
+            echo 'Meilleures ventes';
+            break;
+        case "printSpeedBW":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Vitesse impression NB';
+            break;
+        case "printSpeedCol":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Vitesse impression Couleur';
+            break;
+        case "scanResolution":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Résolution scan';
+            break;
+        case "topPrice":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo '3 top prix';
+            break;
+        case "priceAndManufacturer":
+            $order;
+            if(isset($_POST["order"]) and !empty($_POST["order"])) {
+                $order = $_POST["order"] == "ascending" ? "ASC" : "DESC";
+            }
+            echo 'Par constructeurs ET prix';
+            break;
+        default;
+            echo 'Valeur inconnue';
+    }
+
+    if (isset($data) and !empty($data)) {
+        $_SESSION["data"] = $data;
+        ?>
+            <ul>
+                <?php foreach ($data as $product) { ?>
+                    <li>
+                        <article>
+                            <div>
+                                <h1><?= $product["proName"] . ': ' . $product["braName"] ?></h1>
+                                <p><?= $product["proManufacturer"] ?></p>
+                                <?php
+                                        switch ($_POST["sorting"]) {
+                                            case "size":
+                                                echo '<p>' . $product["proSize"] . '</p>';
+                                                break;
+                                            case "weight":
+                                                echo '<p>' . $product["proWeight"] . '</p>';
+                                                break;
+                                            case "printSpeedBW":
+                                                echo '<p>' . $product["proPrintSpeedBW"] . '</p>';
+                                                break;
+                                            case "printSpeedCol":
+                                                echo '<p>' . $product["proPrintSpeedCol"] . '</p>';
+                                                break;
+                                            case "scanResolution":
+                                                echo '<p>' . $product["proScanRes"] . '</p>';
+                                                break;
+                                            default;
+                                                echo '';
+                                        }
+                                        ?>
+                                    <p><?= $product["proPrice"] ?></p>
+                                    <p>
+                                        <a href="details.php?idProduct=<?= $product["idProduct"] ?>">Détails</a>
+                                        <a href="consumables.php?idProduct=<?= $product["idProduct"] ?>">Consommables</a>
+                                    </p>
+                            </div>
+                        </article>
+                    </li>
+                <?php } ?>
+            </ul>
+        <?php } else {
+            echo 'Aucune donnée trouvées';
+        }
+        ?>
 </body>
+
 </html>
